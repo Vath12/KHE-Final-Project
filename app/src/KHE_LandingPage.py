902b503c8a125b4638fe8776d5c7b2f37b027fba
Vath12 <henry1791@icloud.com>
2025-04-20 16:45:50 -0700

    make http request to attempt login

--- app/src/KHE_LandingPage.py
+++ app/src/KHE_LandingPage.py
@@ -1,4 +1,7 @@
 import logging
+import requests
+
+import os
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -7,7 +10,9 @@ st.set_page_config(layout = 'wide')
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:  
-    st.text_input("Username")
-    st.text_input("Password", type="password")
-    st.button("Login")
+    st.text_input("Username",max_chars=64,key="input_username")
+    st.text_input("Password", type="password",max_chars=128,key="input_password")
+    if st.button("Login"):
+        results = requests.get(f'http://api:4000/khe/trylogin/{st.session_state["input_username"]}/{st.session_state["input_password"]}')
+        os.write(1,bytes(f'Login Attempt Result code:{results.status_code} content:{results.content}\n',"utf-8"))
 st.session_state['auth_key'] = 0
