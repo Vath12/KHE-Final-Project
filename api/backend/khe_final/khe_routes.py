c5dfd2ceb2341507b26fe7069cad110deb4d34c2
Vath12 <henry1791@icloud.com>
2025-04-20 16:27:31 -0700

    added our DB, try to handle login attempt

---
+++ api/backend/khe_final/khe_routes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db as database
+
+
+# Create a new Blueprint object, which is a collection of routes.
+users = Blueprint('users', __name__)
+
+@users.route('/trylogin/<username>/<password>', methods=['GET','PUT'])
+def get_products(username,password):
+
+    if (len(username) < 64 or len(password)!=64):
+        response.status_code = 99
+        return ""
+
+    # get a cursor object from the database
+    cursor = database.get_db().cursor()
+
+    query = f'''
+        SELECT user_id,password
+        FROM Users
+        WHERE username = '{username}'
+    '''
+
+    cursor.execute(query)
+
+    # The cursor will return the data as a Python Dictionary
+    theData = cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    return response
