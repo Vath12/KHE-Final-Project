b12a5fd49cd8b1b2e24c11cb7c73e6b15ef59c0f
e1hqn <ethanlin2253@gmail.com>
2025-04-20 16:32:04 -0700

    login made
    made login

--- app/src/KHE_LandingPage.py
+++ app/src/KHE_LandingPage.py
@@ -5,5 +5,9 @@ import streamlit as st
 
 st.set_page_config(layout = 'wide')
 
-st.title(f"KHEsoft")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:  
+    st.text_input("Username")
+    st.text_input("Password", type="password")
+    st.button("Login")
 st.session_state['auth_key'] = 0
