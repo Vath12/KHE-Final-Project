29b5a8209fa5c6e473f6f2349fcab5448d9e975a
Vath12 <henry1791@icloud.com>
2025-04-22 19:29:28 -0700

    Implement safe page redirect on http request failure

--- app/src/util/request.py
+++ app/src/util/request.py
@@ -1,13 +1,27 @@
 import streamlit as st
 import requests
 
+EXCEPTION_REDIRECT = "Login"
+
+def safeRequest(URL):
+    """
+    Tries to get data from the input URL, if the request fails it switches to safe error page specified by
+    EXCEPTION_REDIRECT
+    """
+    try:
+        return requests.get(URL)
+    except requests.exceptions.HTTPError:
+        st.switch_page(EXCEPTION_REDIRECT)
+        return None
+    
+
 def getUserInfo() -> dict:
     """
     :rtype: dict
     :return:
     {username,first_name,last_name,email,bio}
     """
-    result = requests.get(f"http://api:4000/userinfo/{st.session_state.get('session_key')}")
+    result = safeRequest(f"http://api:4000/userinfo/{st.session_state.get('session_key')}")
     return result.json()[0] #the query returns an array with one dictionary
 
 def getClassList() -> list[dict]:
@@ -16,7 +30,7 @@ def getClassList() -> list[dict]:
     :return:
     [{class_id,name}]
     """
-    result = requests.get(f"http://api:4000/classlist/{st.session_state.get('session_key')}")
+    result = safeRequest(f"http://api:4000/classlist/{st.session_state.get('session_key')}")
     return result.json()
 
 def getClassInfo(class_id : int) -> dict:
@@ -25,7 +39,7 @@ def getClassInfo(class_id : int) -> dict:
     :return:
     {class_id,name,description,organization}
     """
-    result = requests.get(f"http://api:4000/classinfo/{st.session_state.get('session_key')}/{class_id}")
+    result = safeRequest(f"http://api:4000/classinfo/{st.session_state.get('session_key')}/{class_id}")
     return result.json()[0]
 
 def getNotifications() -> list[dict]:
@@ -34,7 +48,7 @@ def getNotifications() -> list[dict]:
     :return:
     {notification_date,assignment_name,class_name}
     """
-    result = requests.get(f"http://api:4000/notifications/{st.session_state.get('session_key')}")
+    result = safeRequest(f"http://api:4000/notifications/{st.session_state.get('session_key')}")
     return result.json()
 
 def getAnnouncements(class_id : int) -> list[dict]:
@@ -43,7 +57,7 @@ def getAnnouncements(class_id : int) -> list[dict]:
     :return:
     [{author_id,title,message,date_posted}]
     """
-    result = requests.get(f"http://api:4000/announcements/{st.session_state.get('session_key')}/{class_id}")
+    result = safeRequest(f"http://api:4000/announcements/{st.session_state.get('session_key')}/{class_id}")
     return result.json()
 
 def getAssignments(class_id : int) -> list[dict]:
@@ -52,7 +66,7 @@ def getAssignments(class_id : int) -> list[dict]:
     :return:
     [{assignment_id,due_date,name,overall_weight}]
     """
-    result = requests.get(f"http://api:4000/assignments/{st.session_state.get('session_key')}/{class_id}")
+    result = safeRequest(f"http://api:4000/assignments/{st.session_state.get('session_key')}/{class_id}")
     return result.json()
 
 def getAssignmentDetails(class_id : int,assignment_id : int) -> list[dict]:
@@ -61,7 +75,7 @@ def getAssignmentDetails(class_id : int,assignment_id : int) -> list[dict]:
     :return:
     [{name,value,weight}]
     """
-    result = requests.get(f"http://api:4000/assignmentDetails/{st.session_state.get('session_key')}/{class_id}/{assignment_id}")
+    result = safeRequest(f"http://api:4000/assignmentDetails/{st.session_state.get('session_key')}/{class_id}/{assignment_id}")
     return result.json()
 
 def getGrade(class_id : int,assignment_id : int) -> list[dict]:
@@ -70,6 +84,6 @@ def getGrade(class_id : int,assignment_id : int) -> list[dict]:
     :return:
     [{name,grade,value,weight}]
     """
-    result = requests.get(f"http://api:4000/grade/{st.session_state.get('session_key')}/{class_id}/{assignment_id}")
+    result = safeRequest(f"http://api:4000/grade/{st.session_state.get('session_key')}/{class_id}/{assignment_id}")
     return result.json()
 
--- app/src/util/verification.py
+++ app/src/util/verification.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from request import safeRequest
 import requests
 
 def isValidSession():
+    """
+    Verifies that the streamlit session state has a valid session_key, redirects to login on failure
+    """
     if (st.session_state.get("session_key")==None):
         st.switch_page("Login.py")
-    result = requests.get(f"http://api:4000/isValidSession/{st.session_state.get('session_key')}")
+    result = safeRequest(f"http://api:4000/isValidSession/{st.session_state.get('session_key')}")
     if (result.status_code != 200):
         st.switch_page("Login.py")
     return True
