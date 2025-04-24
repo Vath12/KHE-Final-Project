896ef8123f911f4d9a8cf251758c0195421b8528
Vath12 <henry1791@icloud.com>
2025-04-24 15:54:23 -0700

    notification silencing

--- api/backend/blueprints/notification.py
+++ api/backend/blueprints/notification.py
@@ -8,7 +8,7 @@ from backend.blueprints.util import *
 
 notifications = Blueprint('notifications', __name__)
 
-@notifications.route('/notifications/<session_key>')
+@notifications.route('/notifications/<session_key>',methods = ["GET","DELETE"])
 def get_notifications(session_key):
     cursor = database.get_db().cursor()
 
@@ -16,11 +16,19 @@ def get_notifications(session_key):
 
     if (user_id == -1):
         return respond("",CODE_ACCESS_DENIED)
-    
-    query = '''
-        SELECT N.notification_date,A.name as assignment_name,C.name as class_name FROM 
-        (Assignments as A JOIN Notifications as N) JOIN Classes as C WHERE student_id = %s
-    '''
-    success = cursor.execute(query,(user_id))
-    result = cursor.fetchall()
-    return respond(jsonify(result),CODE_SUCCESS)
+    args = request.json(Force = True)
+    if (request.method == "GET"):
+        query = '''
+            SELECT N.notification_date,A.name as assignment_name,C.name as class_name,A.assignment_id FROM 
+            (Assignments as A JOIN Notifications as N) JOIN Classes as C WHERE student_id = %s
+        '''
+        success = cursor.execute(query,(user_id))
+        result = cursor.fetchall()
+        return respond(jsonify(result),CODE_SUCCESS)
+    if (request.method == "DELETE"):
+        query = '''
+           DELETE FROM Notifications WHERE student_id = %s AND assignment_id IN %s
+        '''
+        cursor.execute(query,(user_id,args.get("removeList",[])))
+        database.get_db().commit()
+        return respond("",CODE_SUCCESS)
