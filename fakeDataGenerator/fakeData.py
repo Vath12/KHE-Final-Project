d36d561b10201d034b242a1ba6e8674e76da2f7e
Vath12 <henry1791@icloud.com>
2025-04-24 20:35:30 -0700

    fix

--- fakeDataGenerator/fakeData.py
+++ fakeDataGenerator/fakeData.py
@@ -3,8 +3,8 @@ import hashlib,faker,random
 outputFile = 'boostrap.sql'
 
 
-numUsers = 500
-numCourses = 40
+numUsers = 50
+numCourses = 10
 
 users = 'INSERT INTO Users (username,password,first_name,last_name,email,bio) VALUES\n'
 fake = faker.Faker()
@@ -16,15 +16,15 @@ for i in range(numUsers):
     email = fake.email()
     bio = fake.text()
     users+=f"#password = {password}\n"
-    users+=f"('{uname}','{fname}',{lname},0x{hashlib.sha256(password.encode('utf-8')).hexdigest()},'{email}','{bio}'),\n"
-users = users.rstrip(",")+";"
+    users+=f"('{uname}',0x{hashlib.sha256(password.encode('utf-8')).hexdigest()},'{fname}','{lname}','{email}','{bio}'),\n"
+users = users.rstrip(",\n")+";"
 
 
 profileLinks = "INSERT INTO UserProfileLinks VALUES\n"
 for i in range(numUsers):
     for k in range(random.randint(0,3)):
         profileLinks+=f"({i+1},{k},'{fake.user_name()}'),\n"
-profileLinks = profileLinks.rstrip(",")+";"
+profileLinks = profileLinks.rstrip(",\n")+";"
 
 
 values = list("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890")
@@ -37,7 +37,7 @@ def intToJoinCode(number):
 
 classes = "INSERT INTO Classes VALUES\n"
 codes = []
-for i in range(40):
+for i in range(1,numCourses+1):
     unique = False
     code = ""
     while (not unique):
@@ -48,8 +48,8 @@ for i in range(40):
                 unique = False
                 break
     codes.append(code)
-    classes+=f"({i},'{fake.word()}','{fake.sentence()}','{codes[-1]}')\n"
-classes = classes.rstrip(",")+";"
+    classes+=f"({i},'{fake.word()}','{fake.sentence()}','{codes[-1]}'),\n"
+classes = classes.rstrip(",\n")+";"
 
 memberships = "INSERT INTO Memberships VALUES\n"
 members = []
@@ -58,11 +58,11 @@ ADMIN = 0b11111111
 TA = 0b01000111
 DEFAULT = 0b00000000
 #professors
-for i in range(numCourses):
+for i in range(1,numCourses+1):
     assignments.append([])
     members.append((i,i,ADMIN,1))
 #students
-for i in range(numCourses,numUsers):
+for i in range(numCourses+1,numUsers+1):
     enrolled = []
     for k in range(random.randint(3,8)):
         #user_id course_id perms
@@ -74,14 +74,28 @@ for i in range(numCourses,numUsers):
 
 for m in members:
     memberships+=f"({m[0]},{m[1]},{m[2]},{m[3]}),\n"
-memberships = memberships.rstrip(',')+';'
+memberships = memberships.rstrip(',\n')+';'
 
-id = 0
+work = "INSERT INTO Assignments VALUES\n"
+criteria = "INSERT INTO AssignmentCriteria VALUES\n"
+id = 1
+id2 = 1
 for i in range(numCourses):
-    assignments[i].append([i,id,
-                           fake.sentence(),
-                           fake.date(),random.randint(5,100)/100.0])
-    id+=1
+    for k in range(random.randint(10,20)):
+        assignments[i].append([i,id,
+                            fake.sentence(),
+                            f'2025-{random.randint(1,5)}-{random.randint(0,30)} {random.randint(0,23)}:{random.randint(0,59)}:{random.randint(0,59)}',
+                            random.randint(5,100)/100.0])
+        for z in range(random.randint(1,5)):
+            criteria += f"({id2},{i},{id},'{fake.sentence()}',{random.randint(0,100)},{random.randint(10,100)/100.0}),\n"
+            id2+=1
+        id+=1
+
+for c in assignments:
+    for a in c:
+        work += f"({a[0]},{a[1]},'{a[2]}','{a[3]}',{a[4]}),\n"
+work = work.rstrip(",\n") + ";"
+criteria = criteria.rstrip(",\n") + ";"
 
 data = f'''
 Use gradebook;
@@ -89,6 +103,8 @@ Use gradebook;
 {profileLinks}
 {classes}
 {memberships}
+{work}
+{criteria}
 '''
 
 with open(outputFile,"w+") as f:
@@ -96,6 +112,22 @@ with open(outputFile,"w+") as f:
 
 '''
 Use gradebook;
+
+INSERT INTO Users (username,password,first_name,last_name,email,bio) VALUES
+#password is hashed with SHA-256 the first one is "password"
+('hamburger',0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8,'Alice','Johnson',NULL,'Professor @ Northeastern'),
+#password is "password123"
+('hotdog',0xEF92B778BAFE771E89245B89ECBC08A44A4E166C06659911881F383D4473E94F,'Bob','Kentucky',NULL,NULL),
+#password is silk150
+('textile',0x1B9EE937A8C27DD8C62A711DA8833C2B0DA96442215F58A59F02561AFA52B4DD,'Cilly','String','verySilly@aol.com',NULL);
+
+INSERT INTO UserProfileLinks VALUES
+(1,0,'aj1111'),
+(1,1,'alicej11'),
+(2,3,'b0bBK');
+
+INSERT INTO Classes VALUES
+(1,'CS3200','Introduction to Databases','Northeastern University','ABX0101C');
 
 INSERT INTO Memberships VALUES
 (1,1,0b11111111,1),
@@ -115,6 +147,7 @@ INSERT INTO Grades VALUES
 (1,2,10),
 (1,3,0);
 
+
 INSERT INTO Comments VALUES
 (1,1,2,1,'2025-01-20 13:24:05','Good Job!'),
 (2,1,3,1,'2025-01-20 13:30:50','You just had to submit this.');
@@ -123,4 +156,5 @@ INSERT INTO Comments VALUES
 INSERT INTO Notifications VALUES
 (1,2,'2025-01-20 13:24:05'),
 (1,3,'2025-01-20 13:30:50');
+
 '''
